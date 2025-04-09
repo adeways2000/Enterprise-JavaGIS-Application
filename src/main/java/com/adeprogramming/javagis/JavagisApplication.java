59bcf73694510e56ea83e614f4cdf4169c8a85ca
adeways2000 <adeways2000@gmail.com>
2025-04-09 16:58:54 +0200

    first commit - starting project

---
+++ src/main/java/com/adeprogramming/javagis/JavagisApplication.java
@@ -0,0 +1,13 @@
+package com.adeprogramming.javagis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavagisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavagisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adeprogramming/javagis/JavagisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adeprogramming.javagis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavagisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
